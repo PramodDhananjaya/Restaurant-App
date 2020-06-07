8796724560d05682f7bab70b7b4a40b862bc23c4
PramodDhananjaya <pramod.dhananjaya.7@gmail.com>
2020-06-07 18:55:28 +0530

    SPA Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { PromotionService } from '../services/promotion.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
